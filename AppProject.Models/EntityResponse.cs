f735f8ce827071c6e3de6c9ed8dde9baa9c6380c
Joao Victor <Joao.VictorDeSouza@flex.com>
2026-09-21 11:12:24 -0300

    FEAT: Criando Estrutura dos DTOs

---
+++ AppProject.Models/EntityResponse.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace AppProject.Models;
+
+public class EntityResponse
+{
+
+}
--- AppProject.Models/obj/Debug/net10.0/AppProject.Models.AssemblyInfo.cs
+++ AppProject.Models/obj/Debug/net10.0/AppProject.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppProject.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4302a6f6bc5b124c8fc4cd496c2131f337c19bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppProject.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppProject.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
